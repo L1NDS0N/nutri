d0e392f21e379948463861fd5e3b2688a9eb7f0e
Lindson França <44710146+L1NDS0N@users.noreply.github.com>
2023-11-25 20:12:24 +0000

    fix: not building

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,8 @@ import { ImSpinner2 } from "react-icons/im";
 
 export default function Home() {
   const router = useRouter();
-
-  // This component will never be rendered on the server side,
-  // but if for some reason it does, the redirect will be handled.
-  if (typeof window !== "undefined") {
-    router.push("/dashboard");
-  }
-
+  router.push("/dashboard");
+  
   return (
     <div className="flex items-center justify-center h-screen">
       <div>
