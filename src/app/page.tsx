afe86667e2a1168d825619ee7a32945ff0abce77
Lindson França <44710146+L1NDS0N@users.noreply.github.com>
2023-11-25 20:06:38 +0000

    fix: not building

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-'use client'
-import { useRouter } from "next/navigation"
+"use client";
+import { useRouter } from "next/navigation";
+import { ImSpinner2 } from "react-icons/im";
 
 export default function Home() {
   const router = useRouter();
-  router.push("/dashboard");
 
-  return <></>;
+  // This component will never be rendered on the server side,
+  // but if for some reason it does, the redirect will be handled.
+  if (typeof window !== "undefined") {
+    router.push("/dashboard");
+  }
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div>
+        <ImSpinner2 className="animate-spin" />
+      </div>
+    </div>
+  );
 }
