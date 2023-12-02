d90cc49af568ed03eb443823c44de9411c2984c3
Lindson França <44710146+L1NDS0N@users.noreply.github.com>
2023-12-02 18:12:05 +0000

    feat: first version of loading service

---
+++ src/services/api/loading-crud.service.ts
@@ -0,0 +1,49 @@
+import { CoreService } from "./core.service";
+
+export class LoadingCrudService<
+  T extends { id: number | string }
+> extends CoreService {
+  private isLoadingCrud = false;
+
+  constructor({ resource }: { resource?: string }) {
+    super({ resource });
+  }
+
+  index() {
+    this.startLoading();
+    return this.get();
+  }
+
+  storeOne({ data }: { data: Partial<T> }) {
+    this.startLoading();
+    return this.post({ data });
+  }
+
+  updateOne({ data }: { data: Partial<T> }) {
+    this.validateIdPresent(data);
+    this.startLoading();
+    return this.put({ endpoint: `${data.id}`, data });
+  }
+
+  deleteOne({ data }: { data: Partial<T> }) {
+    this.validateIdPresent(data);
+    this.startLoading();
+    return this.delete({ endpoint: `${data.id}` });
+  }
+
+  get isLoading() {
+    return this.isLoadingCrud;
+  }
+
+  private validateIdPresent(data: Partial<T>) {
+    if (!data?.id) throw new Error("ID not present on the data you informed");
+  }
+
+  private startLoading() {
+    this.isLoadingCrud = true;
+  }
+
+  private stopLoading() {
+    this.isLoadingCrud = false;
+  }
+}
