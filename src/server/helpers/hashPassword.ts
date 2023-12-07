d2c092f1319e913f4d413a061fec41f71ef03561
l1nds0n <l1nds0n@hotmail.com>
2023-12-07 12:29:26 -0300

    refact: some adjustments

--- src/server/helpers/hashPassword.ts
+++
@@ -1,6 +0,0 @@
-import bcrypt from 'bcrypt';
-
-export async function hashPassword(plainTextPassword: string): Promise<string> {
-	if (!plainTextPassword) return '';
-	return await bcrypt.hash(plainTextPassword, 10);
-}
