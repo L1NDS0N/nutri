be8860b3c7b5c918cd27054ba2bf52c0d14aff76
Lindson França <44710146+L1NDS0N@users.noreply.github.com>
2023-12-08 00:51:40 +0000

    refact form

--- src/app/nutritional-assessment/page.tsx
+++ src/app/nutritional-assessment/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import XRequiredLabel from "@/components/XRequiredLabel";
 import { NutritionalAssessment } from "@/models/nutritional-assessment.model";
 import { Patient } from "@/models/patient.model";
 import { initialLoadingTypes } from "@/services/api/loading-crud.service";
@@ -6,7 +7,7 @@ import { PatientService } from "@/services/api/patient.service";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "primereact/button";
 import { Dropdown } from "primereact/dropdown";
-import { InputText } from "primereact/inputtext";
+import { InputNumber } from "primereact/inputnumber";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 
@@ -30,34 +31,58 @@ export default function NutritionalAssessmentPage() {
     resolver: zodResolver(NutritionalAssessment),
   });
   const patientField = register("patient_id");
-  console.log(watch());
+
   return (
-    <form className="max-w-xl mx-auto mt-8 p-6 bg-white rounded-md shadow-md">
+    <form className="max-w-3xl mx-auto mt-8 p-6 bg-white rounded-md shadow-md">
       <h2 className="text-2xl font-bold mb-4">
         Formulário de Avaliação Nutricional
       </h2>
 
-      <div className="flex flex-col">
-        <label htmlFor="patient">Selecione um Paciente</label>
-        <Dropdown
-          optionLabel="name"
-          optionValue="id"
-          options={patients}
-          placeholder="Escolha um paciente"
-          ref={patientField.ref}
-          onChange={(e) => setValue("patient_id", e.target.value)}
-          value={watch().patient_id}
-        />
-      </div>
-      <div className="md:flex gap-4">
-        <div className="flex flex-col flex-1">
-          <label htmlFor="weight">Peso (kg)</label>
-          <InputText {...register("weight")} />
+      <div className="flex flex-col ">
+        <div className="flex flex-col ">
+          <XRequiredLabel description="Selecione um Paciente" />
+          <Dropdown
+            name="patient_id"
+            optionLabel="name"
+            optionValue="id"
+            options={patients}
+            placeholder="Escolha um paciente"
+            ref={patientField.ref}
+            onChange={(e) => setValue("patient_id", e.target.value)}
+            value={watch().patient_id}
+          />
+          {errors.patient_id?.message && (
+            <span className="text-red-500">{errors.patient_id?.message}</span>
+          )}
         </div>
+        <div className="md:flex gap-4">
+          <div className="flex flex-col flex-1">
+            <label htmlFor="weight">Peso</label>
+            <InputNumber
+              showButtons
+              suffix="kg"
+              maxFractionDigits={2}
+              name="weight"
+              {...(register("weight") as any)}
+            />
+            {errors.weight?.message && (
+              <span className="text-red-500">{errors.patient_id?.message}</span>
+            )}
+          </div>
 
-        <div className="flex flex-col flex-1">
-          <label htmlFor="height">Altura (cm)</label>
-          <InputText {...register("height")} />
+          <div className="flex flex-col flex-1">
+            <label htmlFor="height">Altura</label>
+            <InputNumber
+              showButtons
+              suffix="m"
+              maxFractionDigits={2}
+              name="height"
+              {...(register("height") as any)}
+            />
+            {errors.patient_id?.message && (
+              <span className="text-red-500">{errors.height?.message}</span>
+            )}
+          </div>
         </div>
       </div>
 
