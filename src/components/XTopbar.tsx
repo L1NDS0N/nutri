53dd423ba5ee29a85aebca5c32dc7ebd163b01fa
l1nds0n <l1nds0n@hotmail.com>
2023-12-10 06:53:41 -0300

    Update XTopbar.tsx

--- src/components/XTopbar.tsx
+++ src/components/XTopbar.tsx
@@ -1,17 +1,17 @@
 export default function XTopbar() {
     return (
-        <header className="bg-gray-700 text-white p-4">
+        <header className="bg-slate-700 text-white p-4">
           <div className="flex justify-between items-center">
             <div>
-              <h1 className="font-bold">Nutrição</h1>
+              <h1 className="font-bold">Unidade de Atendimento Nutricional</h1>
             </div>
             <nav className="sm:block">
               <ul className="flex space-x-4">
                 <li>
-                  <a href="#" className="hover:text-gray-300">Configurações</a>
+                  <a href="#" className="hover:text-slate-300">Configurações</a>
                 </li>
                 <li>
-                  <a href="#" className="hover:text-gray-300">Sair</a>
+                  <a href="#" className="hover:text-slate-300">Sair</a>
                 </li>
               </ul>
             </nav>
