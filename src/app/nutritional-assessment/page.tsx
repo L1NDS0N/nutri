b0cc3ac81d536208558488d0de090905ce7a6461
l1nds0n <l1nds0n@hotmail.com>
2023-12-10 12:16:27 -0300

    feat: imc friendly presentation

--- src/app/nutritional-assessment/page.tsx
+++ src/app/nutritional-assessment/page.tsx
@@ -11,6 +11,8 @@ import { PatientService } from "@/services/api/patient.service";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "primereact/button";
 import { InputNumber } from "primereact/inputnumber";
+import { Knob } from "primereact/knob";
+import { Tooltip } from "primereact/tooltip";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { MdOutlineAssessment } from "react-icons/md";
@@ -35,12 +37,15 @@ export default function NutritionalAssessmentPage() {
     resolver: zodResolver(NutritionalAssessment),
   });
   const { patient_id, weight, height } = watch();
+  const imc = NutritionalAssessment.imcFrom({ height, weight });
+  const {imc_caption, imc_color} = NutritionalAssessment.imcCaptionFrom(imc);
+  console.log({ height, weight, imc });
+
   const [nutriAssmtRequest, setNutriAssmtRequest] =
     useState(initialLoadingTypes);
   const nutriAssmtService = new NutritionalAssessmentService({
     setLoading: setNutriAssmtRequest,
   });
-
   async function handleSaveNutritionalAssessment(data: NutritionalAssessment) {
     nutriAssmtService.storeOne({ data }).then(() => {
       toast.showToast({
@@ -53,68 +58,90 @@ export default function NutritionalAssessmentPage() {
   }
 
   return (
-    <XForm
-      legend="Avaliação Nutricional"
-      icon={<MdOutlineAssessment />}
-      onSubmit={handleSubmit(handleSaveNutritionalAssessment)}
-    >
-      <div className="flex flex-col gap-2">
-        <div className="flex flex-col ">
-          <XRequiredLabel description="Selecione um Paciente" />
-          <XDropdown
-            name="patient_id"
-            optionLabel="name"
-            optionValue="id"
-            options={patients}
-            placeholder="Escolha um paciente"
-            onChange={(e) => setValue("patient_id", e.target.value)}
-            value={patient_id}
-            loading={patientRequest.isLoading}
-          />
-          {errors.patient_id?.message && (
-            <span className="text-red-500">{errors.patient_id?.message}</span>
-          )}
-        </div>
-        <div className="md:flex gap-4">
-          <div className="flex flex-col flex-1">
-            <label htmlFor="weight">Peso</label>
-            <InputNumber
-              showButtons
-              suffix="kg"
-              maxFractionDigits={2}
-              value={weight}
-              onValueChange={(e) => setValue("weight", e.target?.value as any)}
+    <>
+      <Tooltip target=".imc-knob" content={`${imc} - ${imc_caption}`} />
+      
+      <XForm
+        legend="Avaliação Nutricional"
+        icon={<MdOutlineAssessment />}
+        onSubmit={handleSubmit(handleSaveNutritionalAssessment)}
+      >
+        <div className="flex flex-1">
+          <div className="flex flex-col items-center p-4 break-words">
+            <span>IMC</span>
+            <Knob
+              value={imc}
+              min={15}
+              max={50}
+              readOnly
+              className="imc-knob"
+              valueColor={imc_color}
+              strokeWidth={Math.round(4 * (imc / 12))}
             />
-            {errors.weight?.message && (
-              <span className="text-red-500">{errors.weight?.message}</span>
-            )}
-          </div>
-
-          <div className="flex flex-col flex-1">
-            <label htmlFor="height">Altura</label>
-            <InputNumber
-              showButtons
-              suffix="m"
-              maxFractionDigits={2}
-              value={height}
-              onValueChange={(e) => setValue("height", e.target?.value as any)}
-            />
-            {errors.height?.message && (
-              <span className="text-red-500">{errors.height?.message}</span>
-            )}
+            <span>{imc_caption}</span>
           </div>
         </div>
-      </div>
+        <div className="flex flex-col gap-2">
+          <div className="flex flex-col ">
+            <XRequiredLabel description="Selecione um Paciente" />
+            <XDropdown
+              name="patient_id"
+              optionLabel="name"
+              optionValue="id"
+              options={patients}
+              placeholder="Escolha um paciente"
+              onChange={(e) => setValue("patient_id", e.target.value)}
+              value={patient_id}
+              loading={patientRequest.isLoading}
+            />
+            {errors.patient_id?.message && (
+              <span className="text-red-500">{errors.patient_id?.message}</span>
+            )}
+          </div>
+          <div className="md:flex gap-4">
+            <div className="flex flex-col flex-1">
+              <label htmlFor="weight">Peso (kg)</label>
+              <InputNumber
+                showButtons
+                maxFractionDigits={2}
+                value={weight}
+                onValueChange={(e) =>
+                  setValue("weight", e.target?.value as any)
+                }
+              />
+              {errors.weight?.message && (
+                <span className="text-red-500">{errors.weight?.message}</span>
+              )}
+            </div>
 
-      <div className="mt-6">
-        <Button
-          loading={nutriAssmtRequest.isStoring}
-          type="submit"
-          className="w-full justify-center"
-        >
-          Enviar
-        </Button>
-      </div>
-    </XForm>
+            <div className="flex flex-col flex-1">
+              <label htmlFor="height">Altura (m)</label>
+              <InputNumber
+                showButtons
+                maxFractionDigits={2}
+                step={0.1}
+                value={height}
+                onValueChange={(e) =>
+                  setValue("height", e.target?.value as any)
+                }
+              />
+              {errors.height?.message && (
+                <span className="text-red-500">{errors.height?.message}</span>
+              )}
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-6">
+          <Button
+            loading={nutriAssmtRequest.isStoring}
+            type="submit"
+            className="w-full justify-center"
+          >
+            Enviar
+          </Button>
+        </div>
+      </XForm>
+    </>
   );
 }
--- src/models/nutritional-assessment.model.ts
+++ src/models/nutritional-assessment.model.ts
@@ -1,17 +1,49 @@
-import { z } from 'zod';
-import { Z } from 'zod-class';
-import { Patient } from './patient.model';
+import { z } from "zod";
+import { Z } from "zod-class";
+import { Patient } from "./patient.model";
 
 export class NutritionalAssessment extends Z.class({
   id: z.number().optional(),
-  weight: z.number().min(0, 'o valor mínimo é 0').optional(),
-  height: z.number().min(0, 'o valor mínimo é 0').optional(),
+  weight: z.number().min(0, "o valor mínimo é 0").optional(),
+  height: z.number().min(0, "o valor mínimo é 0").optional(),
   created_at: z.date().optional(),
   updated_at: z.date().optional(),
-  patient_id: z.string({ required_error: 'O paciente é obrigatório' }).uuid(),
+  patient_id: z.string({ required_error: "O paciente é obrigatório" }).uuid(),
   patient: Patient.optional(),
 }) {
+  static imcCaptions = [
+    { limit: 0, imc_caption: "", imc_color: "" },
+    { limit: 17, imc_caption: "Muito abaixo do peso", imc_color: "#ef4444" },
+    { limit: 18.5, imc_caption: "Abaixo do peso", imc_color: "#fde047" },
+    { limit: 25, imc_caption: "Peso normal", imc_color: "#84cc16" },
+    { limit: 30, imc_caption: "Acima do peso", imc_color: "#fde047" },
+    { limit: 35, imc_caption: "Obesidade I", imc_color: "#ef4444" },
+    { limit: 40, imc_caption: "Obesidade II (severa)", imc_color: "#dc2626" },
+    { limit: 50, imc_caption: "Obesidade III (mórbida)", imc_color: "#b91c1c" },
+    {
+      limit: Number.POSITIVE_INFINITY,
+      imc_caption: "Obesidade III (mórbida)",
+      imc_color: "#991b1b",
+    },
+  ];
+
   get imc() {
-    if (this.weight && this.height) return this.weight / this.height ** 2;
+    const { height, weight } = this;
+    return NutritionalAssessment.imcFrom({ height, weight });
+  }
+
+  static imcFrom({ height, weight }: { height?: number; weight?: number }) {
+    if (weight && height) return Number((weight / height ** 2).toFixed(2));
+    return 0;
+  }
+
+  static imcCaptionFrom(imc: number) {
+    for (const faixa of this.imcCaptions) {
+      if (imc <= faixa.limit) {
+        return faixa;
+      }
+    }
+
+    return this.imcCaptions[0];
   }
 }
